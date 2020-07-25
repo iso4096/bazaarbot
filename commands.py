b16904963a4c2ca9f5ad1ba0091623b85098821a
BobbyLee <45616468+SveltestBean1@users.noreply.github.com>
2020-07-25 10:59:04 +0800

    Some commands written, swear filter added

--- commands.py
+++ commands.py
@@ -5,8 +5,45 @@ import json
 from urllib import request
 import os
 
+client = discord.Client()
+
 class Bot(object):
-    def __init__(self, client):
-        self.client = client
+    global client
+
+    def __init__(self, apikey):
+        self.prefix = '$'
+        self.apikey = apikey
     
-    #code here
+    def list_in_str(self, a, b):
+        for entry in a:
+            if entry in b:
+                return True
+        return False
+
+    async def cmd(self, message):
+        args = message.content
+
+        #https://data-flair.training/blogs/python-switch-case/
+
+        if not (args.startswith('$')) and self.list_in_str(open('swearfilter.txt', 'r').split('\n'), args):
+            return
+        elif self.list_in_str(open('swearfilter.txt', 'r').split('\n'), args):
+            return self.swear(message)
+        else:
+            method_name = args.split(' ')[0]
+            method = getattr(self, method_name, lambda:'c\'mon mate this isn\'t a command')
+            return method(message)
+
+    def help(self, message):
+        pass
+
+    async def swear(self, message):
+        await message.channel.send('>:(')
+
+    #main
+    @client.event
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+        
+        self.cmd(message)
