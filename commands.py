53d4c2b01310edd153ed73988f81011665a19343
BobbyLee <45616468+SveltestBean1@users.noreply.github.com>
2020-07-25 17:13:09 +0800

    Update commands.py

--- commands.py
+++ commands.py
@@ -9,13 +9,20 @@ import os
 client = discord.Client()
 other_commands = json.loads(open('othercommands.json', 'r'))
 info = json.loads(open('info.json', 'r'))
+perms = json.loads(open('perms.json', 'r'))
 
 def bazaar_load(key):
     output = json.loads(request.urlopen("https://api.hypixel.net/skyblock/bazaar?key={apikey}".format(apikey=key)).read())
     return output
 
+def get_key(val): 
+    for key, value in replacements.items(): 
+         if val == value.lower(): 
+             return key 
+    return
+
 class Bot(object):
-    global client, other_commands, info
+    global client, other_commands, info, perms
 
     def __init__(self, apikey):
         self.prefix = '$'
@@ -47,8 +54,27 @@ class Bot(object):
     
     async def bazaar(self, message):
         bazaar = bazaar_load(info["hypixel_api_key"])
+        args = get_key(message.content[8:].lower())
+        try:
+            product = bazaar["products"][args]["quick_status"]
+            await message.channel.send('__{item}__\nBuy price: {buy} coins\nSell price: {sell} coins\nMargin: {margin} coins'.format(item=replacements[args], buy=round(product["buyPrice"], 2), sell=round(product["sellPrice"], 2), margin=round(product["buyPrice"]-product["sellPrice"], 2)))
+        except KeyError:
+            await message.channel.send('Invalid item.')
 
+    async def isplayer(self, message):
+        try:
+            username = json.loads(request.urlopen('https://api.mojang.com/users/profiles/minecraft/{username}'.format(username=message.content[10:])).read())
+            await message.channel.send('{username} is a player.\nhttps://crafatar.com/renders/body/{uuid}?scale=5'.format(username=username['name'],uuid=username['id']))
+        except:
+            await message.channel.send('{username} is not a player.'.format(username=message.content[9:]))
 
+    async def close(self, message):
+        if message.author.name in perms["close"]:
+            await client.close()
+    
+    async def echo(self, message):
+        await message.channel.send(message.content[5:])
+    
     async def swear(self, message):
         await message.channel.send('>:(')
 
