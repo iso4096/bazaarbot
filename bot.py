492fbe94c673717c07be098bea3c675143385cf2
BobbyLee <45616468+SveltestBean1@users.noreply.github.com>
2020-07-25 10:33:06 +0800

    Created commands.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6 version 3.6
+#!/usr/bin/python3
 
 import discord
 import json
---
+++ commands.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import discord
+import json
+from urllib import request
+import os
+
+class Bot(object):
+    def __init__(self, client):
+        self.client = client
+    
+    #code here
