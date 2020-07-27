5bd5fd2c142d8148f3fad273bc07cb5054937f99
BobbyLee <45616468+SveltestBean1@users.noreply.github.com>
2020-07-27 21:00:58 +0800

    more juice

--- commands.py
+++ commands.py
@@ -48,7 +48,7 @@ class Bot(object):
             await message.channel.send(other_commands["matches_exactly"][args])
         elif args.startswith(self.prefix):
             method_name = args.split(' ')[0][len(self.prefix):]
-            await getattr(self, method_name, lambda _:'c\'mon mate this isn\'t a command')(message)
+            await getattr(self, method_name, lambda _: message.channel.send('c\'mon mate this isn\'t a command'))(message)
 
     async def help(self, message):
         await message.channel.send("not implemented because i'm too lazy")
